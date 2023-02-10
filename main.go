2cda8fbd37d581272913ceb68fb3ba0952193c09
Rahmat Setiawan <setiawaneggy@gmail.com>
2023-02-10 10:51:34 +0700

    success send email

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-mail/mail"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	m := mail.NewMessage()
+
+	m.SetHeader("From", "rahmat.setiawan@lawencon.com")
+
+	m.SetHeader("To", "setiawaneggy@gmail.com", "rahmat.shopee2@gmail.com", "ramadayantitiara@gmail.com")
+
+	// m.SetAddressHeader("Cc", "oliver.doe@example.com", "Oliver")
+
+	m.SetHeader("Subject", "Email Blast")
+
+	m.SetBody("text/html", "Hello <b>Tiara</b> ini testing email blast ya!")
+
+	// m.Attach("lolcat.jpg")
+
+	d := mail.NewDialer("srv42.niagahoster.com", 465, "rahmat.setiawan@lawencon.com", "blacksettings2")
+
+	// Send the email to Kate, Noah and Oliver.
+
+	if err := d.DialAndSend(m); err != nil {
+
+		panic(err)
+
+	}
+
 }
